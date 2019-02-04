dabeb6d97845f40f2ee6dc7969ec51d9d68f05e1
kodyclemens <kody.clemens@gmail.com>
2019-02-04 17:00:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code in this file!
+function scuberGreetingForFeet(num) {
+  if (num <= 400) {
+    return 'This one is on me!';
+  } else if (num > 2500) {
+    return 'No can do.';
+  } else if (num > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+}
+
+function ternaryCheckCity(city) {
+  let result;
+  city === 'NYC' ? (result = 'Ok, sounds good.') : (result = 'No go.');
+  return result;
+}
+
+function switchOnCharmFromTip(tip) {
+  let result;
+  switch(tip) {
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    case 'thanks for everything':
+      result = 'Bye.'
+      break;
+  }
+  return result;
+}
